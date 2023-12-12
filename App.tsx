e82900998355baa8dba283b35a65f90af6a3c9a5
larezelliot <elliotlarez02@gmail.com>
2023-12-12 01:11:46 -0500

    Added Spotify End-User Authentication

--- App.tsx
+++ App.tsx
@@ -6,11 +6,53 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen'
 import { ProfileScreen } from './src/screens/ProfileScreen';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 
 const Stack = createNativeStackNavigator();
 
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
+
 export default function App() {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: 'e4ed6c256030409b9fd88bef2a3db6e6', // Client ID (Webify)
+      scopes: ['user-read-email', 'playlist-modify-public'],  // Permissions
+      usePKCE: false,
+      redirectUri: makeRedirectUri({
+        scheme: 'webify',
+        path: 'callback'
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+    }
+  }, [response]);
+
   return (
+    <View>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
+      <Text>
+        {String(response?.type)}
+      </Text>
+    </View>
+    /*
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
@@ -23,7 +65,7 @@ export default function App() {
           component={ProfileScreen}
           options={{ title: 'Profile' }} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer>*/
   );
 }
 
