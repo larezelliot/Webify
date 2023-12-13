cb115e09cdbd41d148eff5182c7267bb03f8c953
larezelliot <elliotlarez02@gmail.com>
2023-12-13 16:17:44 -0500

    Added Greeting user 'DisplayName' in HomeScreen

--- App.tsx
+++ App.tsx
@@ -6,24 +6,45 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen'
 import { ProfileScreen } from './src/screens/ProfileScreen';
+import { createContext, useState } from 'react';
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
+export const SpotifyAPIContext = createContext<SpotifyApi | undefined>(undefined);
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [spotifyAPI, setSpotifyAPI] = useState(
+    SpotifyApi.withUserAuthorization(
+      "e4ed6c256030409b9fd88bef2a3db6e6",
+      "https://localhost:19006/callback",
+      [
+        "user-read-email",
+        "playlist-modify-public",
+        "user-top-read",
+        "playlist-modify-public",
+        "playlist-modify-private",
+        "user-follow-read",
+        "user-follow-modify"
+      ])
+  );
+  spotifyAPI.authenticate();
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home' }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: 'Profile' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SpotifyAPIContext.Provider value={spotifyAPI}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Home' }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{ title: 'Profile' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SpotifyAPIContext.Provider>
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,30 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { SpotifyAPIContext } from '../../App';
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
 
 export function HomeScreen({ navigation }: any) {
+    const spotifyAPI = useContext(SpotifyAPIContext);
+
+    const [name, setName] = useState<string | undefined>(undefined);
+
+    const fetchData = async () => {
+        try {
+            const fetchedName = (await spotifyAPI?.currentUser.profile())?.display_name;
+
+            setName(fetchedName);
+        } catch (error) { console.error('Error fetching data:', error); }
+    };
+    useEffect(() => { fetchData(); }, []);
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
+            <Text>Hello, {name}</Text>
             <Button
                 title="Go to Profile"
                 onPress={
